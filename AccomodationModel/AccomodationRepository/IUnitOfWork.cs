6e71096c205aa8c4ac7b693ca01257d5c2e767b0
Bertold <agostonberti@gmail.com>
2023-04-19 21:25:17 +0200

    Add Unit of work

---
+++ AccomodationModel/AccomodationRepository/IUnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using AccomodationModel.AccomodationRepository.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccomodationModel.AccomodationRepository
+{
+    public interface IUnitOfWork
+    {
+        public AllergenicRepository AllergenicRepository { get; }
+        public EquipmentRepository EquipmentRepository { get; }
+        public FoodRepository FoodRepository { get; }
+        public GuestRepository GuestRepository { get; }
+        public MealRepository MealRepository { get; }
+        public ReservationRepository ReservationRepository { get; }
+        public RoomPriceRepository RoomPriceRepository { get; }
+        public RoomRepository RoomRepository { get; }
+        public RoomTypeRepository RoomTypeRepository { get; }
+        public ServiceRepository ServiceRepository { get; }
+        Task SaveAsync();
+    }
+}
---
+++ AccomodationModel/AccomodationRepository/UnitOfWork.cs
@@ -0,0 +1,51 @@
+﻿using AccomodationModel.AccomodationRepository.Repositories;
+using AccomodationModel.Models;
+
+namespace AccomodationModel.AccomodationRepository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AccomodationContext context;
+
+        private AllergenicRepository _allergenicRepository;
+        private EquipmentRepository _equipmentRepository;
+        private FoodRepository _foodRepository;
+        private GuestRepository _guestRepository;
+        private MealRepository _mealRepository;
+        private ReservationRepository _reservationRepository;
+        private RoomPriceRepository _roomPriceRepository;
+        private RoomRepository _roomRepository;
+        private RoomTypeRepository _roomTypeRepository;
+        private ServiceRepository _serviceRepository;
+
+        public UnitOfWork(AccomodationContext context)
+        {
+            this.context = context;
+        }
+
+        public AllergenicRepository AllergenicRepository => _allergenicRepository ?? new AllergenicRepository(context);
+
+        public EquipmentRepository EquipmentRepository => _equipmentRepository ?? new EquipmentRepository(context);
+
+        public FoodRepository FoodRepository => _foodRepository ?? new FoodRepository(context);
+
+        public GuestRepository GuestRepository => _guestRepository ?? new GuestRepository(context);
+
+        public MealRepository MealRepository => _mealRepository ?? new MealRepository(context);
+
+        public ReservationRepository ReservationRepository => _reservationRepository ?? new ReservationRepository(context);
+
+        public RoomPriceRepository RoomPriceRepository => _roomPriceRepository ?? new RoomPriceRepository(context);
+
+        public RoomRepository RoomRepository => _roomRepository ?? new RoomRepository(context);
+
+        public RoomTypeRepository RoomTypeRepository => _roomTypeRepository ?? new RoomTypeRepository(context);
+
+        public ServiceRepository ServiceRepository => _serviceRepository ?? new ServiceRepository(context);
+
+        public async Task SaveAsync()
+        {
+            await context.SaveChangesAsync();
+        }
+    }
+}
