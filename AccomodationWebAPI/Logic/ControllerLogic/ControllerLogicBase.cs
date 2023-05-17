d46064184596e000e10241a65785eed86a6f4d30
Bertold <agostonberti@gmail.com>
2023-05-17 20:54:32 +0200

    Add ControllerLogicBase

---
+++ AccomodationWebAPI/Logic/ControllerLogic/ControllerLogicBase.cs
@@ -0,0 +1,38 @@
+﻿using AccomodationModel.AccomodationRepository;
+using AccomodationModel.Models;
+using AccomodationWebAPI.DTOs;
+using AccomodationWebAPI.Logic.Factories;
+using AutoMapper;
+
+namespace AccomodationWebAPI.Logic.ControllerLogic
+{
+    public abstract class ControllerLogicBase<TGetDTO, TPostDTO, TPutDTO>
+    {
+        protected readonly IUnitOfWork unitOfWork;
+        protected readonly IMapper mapper;
+        protected readonly IPagingFactroy pagingFactroy;
+
+        public ControllerLogicBase(IUnitOfWork unitOfWork, IMapper mapper, IPagingFactroy pagingFactroy)
+        {
+            this.unitOfWork = unitOfWork;
+            this.mapper = mapper;
+            this.pagingFactroy = pagingFactroy;
+        }
+
+        public abstract Task<PagingDTO<TGetDTO>> GetAllAsync(int pageNumber, int pageSize);
+        public abstract Task<TGetDTO> GetAsync(int id);
+        public abstract Task CreateAsync(TPostDTO entity);
+        public abstract Task DeleteAsync(int id);
+        public abstract Task<TGetDTO> UpdateAsync(TPutDTO entity);
+
+        protected void SetFieldsForUpdate<T>(T sourceEntity, T updateEntity)
+        {
+            foreach (var prop in typeof(T).GetProperties())
+            {
+                var propertyValue = prop.GetValue(updateEntity);
+                if (propertyValue != null)
+                    prop.SetValue(sourceEntity, propertyValue);
+            }
+        }
+    }
+}
