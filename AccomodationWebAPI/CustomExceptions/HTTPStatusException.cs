be71511862fb84ff3e9e686bca1e2a25ee4858ff
Bertold <agostonberti@gmail.com>
2023-05-17 20:53:48 +0200

    Add HttpStatusException

---
+++ AccomodationWebAPI/CustomExceptions/HTTPStatusException.cs
@@ -0,0 +1,23 @@
+﻿using System.Net;
+
+namespace AccomodationWebAPI.CustomExceptions
+{
+    public class HTTPStatusException : Exception
+    {
+        public HTTPStatusException() { }
+
+        public HTTPStatusException(string message, HttpStatusCode status)
+            : base(message) 
+        {
+            StatusCode = status;
+        }
+
+        public HTTPStatusException(string message, Exception inner, HttpStatusCode status)
+            : base(message, inner) 
+        {
+            StatusCode = status;
+        }
+
+        public HttpStatusCode StatusCode { get; set; }
+    }
+}
