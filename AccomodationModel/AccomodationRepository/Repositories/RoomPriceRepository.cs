f32f199467846f8d4cd87e5c4812081e188157be
Bertold <agostonberti@gmail.com>
2023-07-09 17:13:57 +0200

    Add set priodes at RoomPrice create

--- AccomodationModel/AccomodationRepository/Repositories/RoomPriceRepository.cs
+++ AccomodationModel/AccomodationRepository/Repositories/RoomPriceRepository.cs
@@ -7,5 +7,12 @@ namespace AccomodationModel.AccomodationRepository.Repositories
         public RoomPriceRepository(AccomodationContext context) : base(context)
         {
         }
+
+        public override async Task CreateAsync(RoomPrice entity)
+        {
+            var roomTpyeRelatedPrices = context.RoomPrices.Where(rp => rp.RoomTypeId == entity.RoomTypeId);
+            entity.SetPeriod(roomTpyeRelatedPrices);
+            await base.CreateAsync(entity);
+        }
     }
 }
---
+++ AccomodationModel/ModelsPartial/RoomPrice.cs
@@ -0,0 +1,19 @@
+﻿namespace AccomodationModel.Models;
+public partial class RoomPrice
+{
+    public void SetPeriod(IEnumerable<RoomPrice> roomTypeRoomPrices)
+    {
+        DateTime currentDate = DateTime.Now;
+        if (roomTypeRoomPrices == null || roomTypeRoomPrices.Count() == 0)
+        {
+            this.PeriodStrart = currentDate;
+            this.PeriodEnd = null;
+        }
+        else
+        {
+            RoomPrice latest = roomTypeRoomPrices.OrderByDescending(rp => rp.PeriodStrart).First();
+            latest.PeriodEnd = currentDate.AddSeconds(-1);
+            this.PeriodStrart = currentDate;
+        }
+    }
+}
