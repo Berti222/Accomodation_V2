df0b882abb525330da0e280deb688cac0540c3bc
Bertold <agostonberti@gmail.com>
2023-07-09 17:22:03 +0200

    Add RoomPriceDTOs

---
+++ AccomodationWebAPI/DTOs/GetDTOs/RoomPriceDTO.cs
@@ -0,0 +1,23 @@
+﻿using AccomodationModel.Models;
+
+namespace AccomodationWebAPI.DTOs.GetDTOs
+{
+    public class RoomPriceDTO : IDTOWithId
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal? Price { get; set; }
+
+        public DateTime? CreatedOn { get; set; }
+
+        public decimal? ExtraBedPrice { get; set; }
+
+        public DateTime? PeriodStrart { get; set; }
+
+        public DateTime? PeriodEnd { get; set; }
+        public int? RoomTypeId { get; set; }
+
+    }
+}
---
+++ AccomodationWebAPI/DTOs/PutDTOs/RoomPricePutDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace AccomodationWebAPI.DTOs.PutDTOs
+{
+    public class RoomPricePutDTO
+    {
+        public string Name { get; set; }
+        public decimal? Price { get; set; }
+        public decimal? ExtraBedPrice { get; set; }
+        public int RoomTypeId { get; set; }
+    }
+}
