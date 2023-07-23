f753bec44a56af6698c9bf9fd973a20e88daaf17
Bertold <agostonberti@gmail.com>
2023-07-23 13:16:01 +0200

    Add RoomTypeDTOs

---
+++ AccomodationWebAPI/DTOs/GetDTOs/RoomTypeDTO.cs
@@ -0,0 +1,19 @@
+﻿using AccomodationModel.Models;
+
+namespace AccomodationWebAPI.DTOs.GetDTOs
+{
+    public class RoomTypeDTO : IDTOWithId
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int? Size { get; set; }
+        public int? NumberOfBed { get; set; }
+        public string Direction { get; set; }
+        public string Discription { get; set; }
+        public string Type { get; set; }
+        public string ImageUrl { get; set; }
+        public byte? Capacity { get; set; }
+        public short? NumberOfRoom { get; set; }
+        public RoomPriceDTO Price { get; set; }
+    }
+}
---
+++ AccomodationWebAPI/DTOs/PostDTOs/RoomTypePostDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace AccomodationWebAPI.DTOs.PostDTOs
+{
+    public class RoomTypePostDTO
+    {
+        public string Name { get; set; }
+        public int? Size { get; set; }
+        public int? NumberOfBed { get; set; }
+        public string Direction { get; set; }
+        public string Discription { get; set; }
+        public string Type { get; set; }
+        public string ImageUrl { get; set; }
+        public byte? Capacity { get; set; }
+    }
+}
